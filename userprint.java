6c7ee7980d12650d031919fb1bf6e117a4491120
Arpan Mallik <130847396+ArpanMallik@users.noreply.github.com>
2025-07-16 20:04:35 +0530

    Add files via upload

---
+++ userprint.java
@@ -0,0 +1,12 @@
+import java . util .Scanner;
+public class userprint 
+{
+    public static void main(String[]args)
+    {
+        Scanner scan = new Scanner(System.in) ;
+        System.out.println("Enter the age ");
+        int age = scan.nextInt();
+        System.out.println("Age is" +  age );
+    }
+    
+}
